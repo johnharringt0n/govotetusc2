6ae1caafedb474ae442029f0699aed67014e741d
John Harrington <john.92.harrington@gmail.com>
2018-04-05 17:43:44 -0700

    One more change to netlify form
    Now using fetch in the `handleSubmit`
    
    Taken from this blog post
    
    https://www.netlify.com/blog/2017/07/20/how-to-integrate-netlifys-form-handling-in-a-react-app/

--- src/components/Mailchimp.js
+++ src/components/Mailchimp.js
@@ -11,7 +11,19 @@ class Mailchimp extends React.Component {
   }
 
   handleChange = event => {
-    this.setState({ email: event.target.value });
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'mailchimpSignup', ...this.state }),
+    })
+      .then(() => alert('Success!'))
+      .catch(error => alert(error));
+
+    event.preventDefault();
   };
 
   render() {
@@ -23,12 +35,17 @@ class Mailchimp extends React.Component {
             Subscribe to our newsletter to recieve updates on voting deadlines,
             ways to get involved, and additional information about the issues.
           </p>
-          <form data-netlify="true" name="mailchimpSignup">
+          <form
+            data-netlify="true"
+            name="mailchimpSignup"
+            onSubmit={this.handleSubmit}
+          >
             <div className="field has-addons">
               <div className="control has-icons-left is-expanded">
                 <input
                   className="input"
                   type="email"
+                  name="email"
                   placeholder="Enter your email"
                   onChange={this.handleChange}
                 />
