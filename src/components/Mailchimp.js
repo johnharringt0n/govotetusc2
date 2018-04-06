e25796929e3e22018af84bd0c96e724d1bd02fe5
John Harrington <john.92.harrington@gmail.com>
2018-04-05 18:19:56 -0700

    Last push didn't include the `encode()` function. Also updated more visual feedback on form submit

--- src/components/Mailchimp.js
+++ src/components/Mailchimp.js
@@ -2,26 +2,68 @@ import React from 'react';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/fontawesome-free-solid';
 
+const SignupText = ({ isSubmitted }) => {
+  if (isSubmitted === true) {
+    return [
+      <h3 key="0" className="has-text-weight-bold">
+        Thanks for signing up!
+      </h3>,
+      <p key="1">
+        We've received your email and we're excited to have you on board.
+      </p>,
+    ];
+  }
+  return [
+    <h3 key="0" className="has-text-weight-bold">
+      Want To Get Involved?
+    </h3>,
+    <p key="1">
+      Subscribe to our newsletter to recieve updates on voting deadlines, ways
+      to get involved, and additional information about the issues.
+    </p>,
+  ];
+};
+
 class Mailchimp extends React.Component {
   constructor() {
     super();
     this.state = {
       email: '',
+      isSubmitted: false,
     };
   }
+
+  encode = data => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&');
+  };
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
   handleSubmit = event => {
-    fetch('/', {
+    const toSend = this.encode({
+      'form-name': 'mailchimpSignup',
+      ...this.state,
+    });
+
+    const data = fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'mailchimpSignup', ...this.state }),
+      body: toSend,
     })
-      .then(() => alert('Success!'))
-      .catch(error => alert(error));
+      .then(() => {
+        this.setState({ isSubmitted: true });
+      })
+      .catch(error => {
+        alert(
+          `Something went wrong while submitting your email address: ${
+            error.message
+          }`
+        );
+      });
 
     event.preventDefault();
   };
@@ -30,11 +72,7 @@ class Mailchimp extends React.Component {
     return (
       <div className="columns is-centered">
         <div className="column is-half-desktop" style={{ textAlign: 'center' }}>
-          <h3 className="has-text-weight-bold">Want To Get Involved?</h3>
-          <p>
-            Subscribe to our newsletter to recieve updates on voting deadlines,
-            ways to get involved, and additional information about the issues.
-          </p>
+          <SignupText isSubmitted={this.state.isSubmitted} />
           <form
             data-netlify="true"
             name="mailchimpSignup"
@@ -48,6 +86,7 @@ class Mailchimp extends React.Component {
                   name="email"
                   placeholder="Enter your email"
                   onChange={this.handleChange}
+                  disabled={this.state.isSubmitted}
                 />
                 <span className="icon is-small is-left">
                   <FontAwesomeIcon icon={faEnvelope} />
