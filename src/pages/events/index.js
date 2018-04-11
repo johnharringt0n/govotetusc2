95882000c6bf65ad3cd1804896b1debbc9ce6508
John Harrington <john.92.harrington@gmail.com>
2018-04-10 19:35:18 -0700

    Updated home page image to be responsive and much smaller size

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -19,16 +19,20 @@ function Events(props) {
               page below.
             </p>
 
-            <div className="center-row">
-              <iframe
-                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fgovotetusc%2F&tabs=events&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=135610003950180"
-                width="500"
-                height="500"
-                style={{ border: 'none', overflow: 'hidden' }}
-                scrolling="no"
-                frameborder="0"
-                allowTransparency="true"
-              />
+            <div className="columns">
+              <div className="column">
+                <div className="center-row">
+                  <iframe
+                    src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fgovotetusc%2F&tabs=events&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=135610003950180"
+                    width="500"
+                    height="500"
+                    style={{ border: 'none', overflow: 'hidden' }}
+                    scrolling="no"
+                    frameborder="0"
+                    allowTransparency="true"
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
